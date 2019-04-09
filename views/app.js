ac2e1d2f88574e42abdefcbf2ab090fd766164cb
Sam Burkhard <samryanburk@gmail.com>
2019-04-09 17:03:21 -0400

    Basic server structure added following MVC model

--- views/app.js
+++ views/app.js
@@ -1,14 +1,9 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const port = 8000
 
-// Global app object
+// Global express instance
 var app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port, () => {
     console.log('Listening on port ${port}')
