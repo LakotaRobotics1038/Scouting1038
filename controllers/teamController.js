325de3ce0f81aeecf446b911c3dfd11f50c5d16c
Sam Burkhard <samryanburk@gmail.com>
2019-04-14 14:50:31 -0400

    Tested and ready for DB design.  Example models, controllers, and routers all implemented

---
+++ controllers/teamController.js
@@ -0,0 +1,6 @@
+var Team = require('../models/team')
+
+/// Team related functions HERE ///
+exports.team_details = function (req, res) {
+  res.send('NOT IMPLEMENTED: Team details')
+}
--- models/team.js
+++ models/team.js
@@ -1,27 +1,20 @@
-var mongoose = reuqire('mongoose')
+var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema
 
 var TeamSchema = new Schema(
   {
-    team_number: { type: Number, required: true },
-    team_rank: { type: Number },
-    driver_skill: {
-      type: Number,
-      min: '1',
-      max: '5',
-      required: true
-    },
-    team_robot: { type: Schema.Types.ObjectID, ref: 'Robot' }
+    team_number: { type: Number, required: true }
   }
+)
 
   // Virtual for team URL
   TeamSchema
   .virtual('url')
   .get(function () {
     return '/teams/' + this._id
-  })
-)
+    }
+  )
 
 // Export Module
 module.exports = mongoose.model('Team', TeamSchema)
