cd76b5c979df4e1256a8330514e8c0e4dc24e49e
Sam Burkhard <samryanburk@gmail.com>
2019-04-17 19:21:31 -0400

    Match routes

--- routes/scouting.js
+++ routes/scouting.js
@@ -11,4 +11,54 @@ var event_controller = require('../controllers/eventController');
 // Get scouting home page
 router.get('/', team_controller.index);
 
+// GET request for creating team
+router.get('/team/create', team_controller.team_create_get);
+
+// POST request for creating team
+router.post('/team/create', team_controller.team_create_post);
+
+// GET request to delete team
+router.get('/team/:id/delete', team_controller.team_delete_get);
+
+// POST request to delete team
+router.post('/team/:id/delete', team_controller.team_delete_post);
+
+// GET request to update team
+router.get('/team/:id/update', team_controller.team_update_get);
+
+// POST request to update team
+router.post('/team/:id/update', team_controller.team_update_post);
+
+// GET request for one team
+router.get('/team/:id', team_controller.team_details);
+
+// GET request for list of all teams
+router.get('/teams', team_controller.team_list);
+
+/// MATCH ROUTES ///
+
+// GET request for creating match
+router.get('/match/create', match_controller.match_create_get);
+
+// POST request for creating match
+router.post('/match/create', match_controller.match_create_post);
+
+// GET request to delete match
+router.get('/match/:id/delete', match_controller.match_delete_get);
+
+// POST request to delete match
+router.post('/match/:id/delete', match_controller.match_delete_post);
+
+// GET request to update match
+router.get('/match/:id/update', match_controller.match_update_get);
+
+// POST request to update match
+router.post('/match/:id/update', match_controller.match_update_post);
+
+// GET request for one match
+router.get('/match/:id', match_controller.match_details);
+
+// GET request for list of all matches
+router.get('/match', match_controller.match_list);
+
 module.exports = router;
