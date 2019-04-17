e3c939f8a0e447b21226e9fa63dbb775959e8a51
Sam Burkhard <samryanburk@gmail.com>
2019-04-17 18:51:43 -0400

    Added Controllers and got routing ready

---
+++ controllers/matchController.js
@@ -0,0 +1,41 @@
+var Match = require('../models/match');
+
+// Display list of all matches
+exports.match_list = function (req, res) {
+  res.send('NOT IMPLEMENTED: match List');
+};
+
+// Display match details page
+exports.match_details = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Details');
+};
+
+// Display match create form on GET
+exports.match_create_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Create GET');
+};
+
+// Handle match create form on POST
+exports.match_create_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Create POST');
+};
+
+// Display match delete form on GET
+exports.match_delete_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Delete GET');
+};
+
+// Handle match delete form on POST
+exports.match_delete_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Delete POST');
+};
+
+// Display match update form on GET
+exports.match_update_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Update GET');
+};
+
+// Handle match update form on POST
+exports.match_update_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: match Update POST');
+};
