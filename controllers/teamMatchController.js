464e50c1ca3d9b271063869b429dadbcfa9d1983
Sam Burkhard <samryanburk@gmail.com>
2019-04-18 21:59:13 -0400

    TeamMatchController added empty methods to handle creation of match

---
+++ controllers/teamMatchController.js
@@ -0,0 +1,31 @@
+var ScoutMatch = require('../models/scoutMatch');
+
+// Display match scouting form
+exports.scoutMatch_create_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Scouting form');
+};
+
+// Handle match scouting form on POST
+exports.scoutMatch_create_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Scouting form post');
+};
+
+// Display scouted match delete form
+exports.scoutMatch_delete_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Delete form');
+};
+
+// Handle scouted match delete form on POST
+exports.scoutMatch_delete_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Delete form POST');
+};
+
+// Display scouted match update form
+exports.scoutMatch_update_get = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Update form');
+};
+
+// Display scouted match update form POST
+exports.scoutMatch_update_post = function (req, res) {
+  res.send('NOT IMPLEMENTED: Match Update form POST');
+};
---
+++ models/scoutMatch.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ScoutMatchSchema = new Schema(
+  {
+    team: { type: Schema.Types.ObjectId, ref: 'Team', required: true },
+    match: { type: Schema.Types.ObjectId, ref: 'Match', required: true},
+    alliance: { type: Schema.Types.ObjectId, ref: 'Alliance', required: true},
+    score: {
+      starting_position: { type: String, enum: ['Hab1', 'Hab2'], required: true},
+      sandstorm: {
+        passed_hab_line: Bool,
+        sandstorm_cargo: Number,
+        sandstorm_hatch: Number
+      },
+      teleop: {
+        teleop_cargo: Number,
+        teleop_hatch: Number
+      },
+      engame: {
+        endgame_level: { type: String, enum: ['None', 'Hab1', 'Hab2', 'Hab3'], required: true }
+      }
+    }
+  }
+);
+
+// Export Module
+module.exports = mongoose.model('ScoutMatch', ScoutMatchSchema);
