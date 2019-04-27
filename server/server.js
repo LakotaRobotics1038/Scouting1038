4d72cdd7613449fcdcfcf9ddde8f6534f67717bb
Sam Burkhard <samryanburk@gmail.com>
2019-04-27 09:48:11 -0400

    Added API route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 // server.js
 
-    // set up ========================
+    // set up ==============================
     var config = require('./config/config')
     var app    = require('./config/express')
     require('./config/mongoose')
+    var apiRouter = require('./routes/api.route.js')
 
-    // start app with node server.js ======================================
+    // routes ==============================
+    app.use('/api', apiRouter)
+
+    // start app with node server.js =======
     app.listen(config.port, () => {
         console.log('server started')
     });
 
-    // export app =====================
+    // export app ===========================
     module.exports = app
